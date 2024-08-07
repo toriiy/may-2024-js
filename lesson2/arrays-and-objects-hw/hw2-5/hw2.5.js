e28ece9d77a1e7969e9706a0064b3550595244ad
toriiy <vikaizhak54@gmail.com>
2024-08-08 01:40:06 +0300

    did the fifth part of hw

---
+++ lesson2/arrays-and-objects-hw/hw2-5/hw2.5.js
@@ -0,0 +1,45 @@
+let monday = {
+    morning: 17,
+    daytime: 23,
+    evening: 19
+}
+
+let tuesday = {
+    morning: 17,
+    daytime: 27,
+    evening: 22
+}
+
+let wednesday = {
+    morning: 19,
+    daytime: 31,
+    evening: 26
+}
+
+let thursday = {
+    morning: 21,
+    daytime: 30,
+    evening: 23
+}
+
+let friday = {
+    morning: 19,
+    daytime: 26,
+    evening: 21
+}
+
+let saturday = {
+    morning: 22,
+    daytime: 29,
+    evening: 25
+}
+let sunday = {
+    morning: 19,
+    daytime: 25,
+    evening: 20
+}
+
+let temperature = [monday, tuesday, wednesday, thursday, friday, saturday, sunday];
+
+console.log(temperature);
+
