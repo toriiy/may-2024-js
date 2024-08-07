11dc9e6cfd764555a9cdce57b305187fd5dc255d
toriiy <vikaizhak54@gmail.com>
2024-08-07 23:24:19 +0300

    did the first part of hw

---
+++ lesson2/arrays-and-objects-hw/hw1-1/hw1.1.js
@@ -0,0 +1,11 @@
+let array = [10, 304, -576, 13.2, 'tomato', true, 3, false, 'sun', 0];
+console.log(array[0]);
+console.log(array[1]);
+console.log(array[2]);
+console.log(array[3]);
+console.log(array[4]);
+console.log(array[5]);
+console.log(array[6]);
+console.log(array[7]);
+console.log(array[8]);
+console.log(array[9]);
