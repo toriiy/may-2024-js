861a0203e970f976e57303a36bd7295faa9e31ac
toriiy <vikaizhak54@gmail.com>
2024-08-08 01:19:10 +0300

    did the fourth part of hw

---
+++ lesson2/arrays-and-objects-hw/hw2-4/hw2.4.js
@@ -0,0 +1,26 @@
+let users = [
+    {name: 'anna', username: 'anna-dd', password: '3ojfj2334'},
+    {name: 'oleg', username: 'oleg-223', password: 'sddj932'},
+    {name: 'andriy', username: 'andri348', password: 'ansad002'},
+    {name: 'ruslan', username: 'lan334', password: 'rull230'},
+    {name: 'bogdan', username: 'bogdann445', password: 'bogfor554'},
+    {name: 'iryna', username: 'ira-n334', password: 'iiraf003'},
+    {name: 'natalya', username: 'nata032', password: 'nana97dt'},
+    {name: 'vasyl', username: 'vasya6757', password: 'avasya343'},
+    {name: 'igor', username: 'iggor44', password: 'iggf334'},
+    {name: 'sofia', username: 'sofi3567', password: 'sosofi58h'}
+]
+
+console.log(users);
+
+console.log(users[0].password);
+console.log(users[1].password);
+console.log(users[2].password);
+console.log(users[3].password);
+console.log(users[4].password);
+console.log(users[5].password);
+console.log(users[6].password);
+console.log(users[7].password);
+console.log(users[8].password);
+console.log(users[9].password);
+
