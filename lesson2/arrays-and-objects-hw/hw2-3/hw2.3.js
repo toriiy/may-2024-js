6909fc6b8de79a3e448aba5489aacc02d70258ae
toriiy <vikaizhak54@gmail.com>
2024-08-08 01:01:26 +0300

    edit

--- lesson2/arrays-and-objects-hw/hw2-3/hw2.3.js
+++ lesson2/arrays-and-objects-hw/hw2-3/hw2.3.js
@@ -2,7 +2,13 @@ let book1 = {
     title: 'Fable',
     pageCount: 320,
     genre: 'fantasy',
-    authors: [{name: 'Annette Marie', age: 32}, {name: 'Li Bardugo', age: 49}, {name: 'Tess Gerritsen', age: 71}, {name: 'Adrienne Young', age: 39}, {name: 'Christina McDonald', age: 42}]
+    authors: [
+        {name: 'Annette Marie', age: 32},
+        {name: 'Li Bardugo', age: 49},
+        {name: 'Tess Gerritsen', age: 71},
+        {name: 'Adrienne Young', age: 39},
+        {name: 'Christina McDonald', age: 42}
+    ]
 }
 
 console.log(book1);
@@ -11,7 +17,13 @@ let book2 = {
     title: 'The Night Olivia Fell',
     pageCount: 416,
     genre: 'detective',
-    authors: [{name: 'Annette Marie', age: 32}, {name: 'Li Bardugo', age: 49}, {name: 'Tess Gerritsen', age: 71}, {name: 'Adrienne Young', age: 39}, {name: 'Christina McDonald', age: 42}]
+    authors: [
+        {name: 'Annette Marie', age: 32},
+        {name: 'Li Bardugo', age: 49},
+        {name: 'Tess Gerritsen', age: 71},
+        {name: 'Adrienne Young', age: 39},
+        {name: 'Christina McDonald', age: 42}
+    ]
 }
 
 console.log(book2);
@@ -20,7 +32,13 @@ let book3 = {
     title: 'Murder in Mesopotamia',
     pageCount: 320,
     genre: 'detective',
-    authors: [{name: 'Annette Marie', age: 32}, {name: 'Li Bardugo', age: 49}, {name: 'Tess Gerritsen', age: 71}, {name: 'Adrienne Young', age: 39}, {name: 'Christina McDonald', age: 42}]
+    authors: [
+        {name: 'Annette Marie', age: 32},
+        {name: 'Li Bardugo', age: 49},
+        {name: 'Tess Gerritsen', age: 71},
+        {name: 'Adrienne Young', age: 39},
+        {name: 'Christina McDonald', age: 42}
+    ]
 }
 
 console.log(book3);
