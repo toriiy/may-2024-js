1e75e6e4be6922a1cc10a77b1a9b8e0814fe895b
toriiy <vikaizhak54@gmail.com>
2024-08-07 23:51:20 +0300

    did the second part of hw

---
+++ lesson2/arrays-and-objects-hw/hw2-2/hw2.2.js
@@ -0,0 +1,23 @@
+let book1 = {
+    title: 'Red Winter',
+    pageCount: 320,
+    genre: 'fantasy'
+}
+
+console.log(book1);
+
+let book2 = {
+    title: 'Six Of Crows',
+    pageCount: 544,
+    genre: 'fantasy'
+}
+
+console.log(book2);
+
+let book3 = {
+    title: 'The Surgeon',
+    pageCount: 350,
+    genre: 'thriller'
+}
+
+console.log(book3);
