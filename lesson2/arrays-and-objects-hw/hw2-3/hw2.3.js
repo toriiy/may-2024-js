3d3395d35506f628290ddc36e3fd1f408a517983
toriiy <vikaizhak54@gmail.com>
2024-08-08 00:37:12 +0300

    did the third part of hw

---
+++ lesson2/arrays-and-objects-hw/hw2-3/hw2.3.js
@@ -0,0 +1,26 @@
+let book1 = {
+    title: 'Fable',
+    pageCount: 320,
+    genre: 'fantasy',
+    authors: [{name: 'Annette Marie', age: 32}, {name: 'Li Bardugo', age: 49}, {name: 'Tess Gerritsen', age: 71}, {name: 'Adrienne Young', age: 39}, {name: 'Christina McDonald', age: 42}]
+}
+
+console.log(book1);
+
+let book2 = {
+    title: 'The Night Olivia Fell',
+    pageCount: 416,
+    genre: 'detective',
+    authors: [{name: 'Annette Marie', age: 32}, {name: 'Li Bardugo', age: 49}, {name: 'Tess Gerritsen', age: 71}, {name: 'Adrienne Young', age: 39}, {name: 'Christina McDonald', age: 42}]
+}
+
+console.log(book2);
+
+let book3 = {
+    title: 'Murder in Mesopotamia',
+    pageCount: 320,
+    genre: 'detective',
+    authors: [{name: 'Annette Marie', age: 32}, {name: 'Li Bardugo', age: 49}, {name: 'Tess Gerritsen', age: 71}, {name: 'Adrienne Young', age: 39}, {name: 'Christina McDonald', age: 42}]
+}
+
+console.log(book3);
