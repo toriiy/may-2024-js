ad7c5def1289da8d8d74717745e459582d595f63
toriiy <vikaizhak54@gmail.com>
2024-08-08 03:14:09 +0300

    did the eighth part of hw

---
+++ lesson2/if-and-switch-hw/hw2-8/hw2.8.js
@@ -0,0 +1,11 @@
+let day = +prompt("Введіть число від 1 до 31");
+
+if (day <= 10) {
+    console.log('Перша декада');
+} else if (day > 10 && day <= 20) {
+    console.log('Друга декада');
+} else if (day > 20 && day <= 31) {
+    console.log('Третя декада');
+} else {
+    console.log('???');
+}
