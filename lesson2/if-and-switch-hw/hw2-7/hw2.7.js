9216447beb3eb59f73af0eae2c9ac4c8041e33fc
toriiy <vikaizhak54@gmail.com>
2024-08-08 03:00:43 +0300

    did the seventh part of hw

---
+++ lesson2/if-and-switch-hw/hw2-7/hw2.7.js
@@ -0,0 +1,13 @@
+let time = +prompt('Напишіть число від 0 до 59');
+
+if (time <= 15) {
+    console.log('Перша чверть години');
+} else if (time > 15 && time <= 30) {
+    console.log('Друга чверть години');
+} else if (time > 30 && time <= 45) {
+    console.log('Третя чверть години');
+} else if (time > 45 && time <= 59) {
+    console.log('Четверта чверть години');
+} else {
+    console.log('???');
+}
