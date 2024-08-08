bab234d022f2d8e0b708bbc427d084c9f29787e5
toriiy <vikaizhak54@gmail.com>
2024-08-08 11:59:10 +0300

    did the tenth part of hw

---
+++ lesson2/if-and-switch-hw/hw2-10/hw2.10.js
@@ -0,0 +1,11 @@
+let x = +prompt('Введіть число');
+let y = +prompt('Введіть число');
+
+if (x > y) {
+    console.log(x);
+} else if (y > x) {
+    console.log(y);
+} else if (x === y) {
+    console.log('Рівні значення');
+}
+
