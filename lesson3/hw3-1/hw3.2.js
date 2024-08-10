f5fdc83dfb74cf1427f8014ce5e7373d311efc44
toriiy <vikaizhak54@gmail.com>
2024-08-11 00:26:26 +0300

    did the first part of hw

---
+++ lesson3/hw3-1/hw3.2.js
@@ -0,0 +1,10 @@
+let array = ['sun', 'sky', 'grass', 'blue', 'book', 'table', 'cloud', 'tree', 'window', 'lamp'];
+console.log(array);
+
+for (let i = 0; i < array.length; i++) {
+    const word = array[i];
+    document.write(`
+    <div>
+    <p>${word}</p>
+    </div>  `);
+}
