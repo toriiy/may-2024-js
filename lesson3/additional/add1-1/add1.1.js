6031b268b7559bc7b7daf7ca6165073804731a96
toriiy <vikaizhak54@gmail.com>
2024-08-11 21:25:07 +0300

    did the first part of additional hw

---
+++ lesson3/additional/add1-1/add1.1.js
@@ -0,0 +1,27 @@
+// --створити масив з:
+// - з 5 числових значень
+// - з 5 стічкових значень
+// - з 5 значень стрічкового, числового та булевого типу
+// - та вивести його в консоль
+
+let arrayNum = [2, 904, 459, -263, 0];
+console.log(arrayNum);
+
+let arrayString = ['sun', 'tree', 'cloud', 'bush', 'grass'];
+console.log(arrayString);
+
+let arrayAny = [5, false, 'rain', -999, true];
+console.log(arrayAny);
+
+console.log('')
+
+// -- Створити пустий масив. Наповнити його будь-якими значеннями звертаючись до конкретного індексу. Вивести в консоль
+
+let arrayEmpty = [];
+arrayEmpty[0] = 7;
+arrayEmpty[1] = true;
+arrayEmpty[2] = 'flower';
+arrayEmpty[3] = 'plant';
+arrayEmpty[4] = -45;
+
+console.log(arrayEmpty);
