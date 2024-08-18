5da83c9419d67d5d9bea818c160f3968a3739ace
toriiy <vikaizhak54@gmail.com>
2024-08-19 01:18:09 +0300

    did the second part of hw

--- lesson6/hw6.js
+++ lesson6/hw6.js
@@ -142,7 +142,7 @@ console.log(mappedArray);
 console.log(coursesAndDurationArray);
 
 console.log('');
-{
+
 
 // описати колоду карт (від 6 до туза без джокерів)
 // - знайти піковий туз
@@ -155,7 +155,190 @@ console.log('');
 //     cardSuit: '', // 'spade', 'diamond','heart', 'clubs'
 //         value: '', // '6'-'10', 'ace','jack','queen','king'
 //     color:'', // 'red','black'
-}
+
+let cards = [
+    {
+        cardSuit: 'spade',
+        value: 6,
+        color: 'black'
+    },
+    {
+        cardSuit: 'spade',
+        value: 7,
+        color: 'black'
+    },
+    {
+        cardSuit: 'spade',
+        value: 8,
+        color: 'black'
+    },
+    {
+        cardSuit: 'spade',
+        value: 9,
+        color: 'black'
+    },
+    {
+        cardSuit: 'spade',
+        value: 10,
+        color: 'black'
+    },
+    {
+        cardSuit: 'spade',
+        value: 'jack',
+        color: 'black'
+    },
+    {
+        cardSuit: 'spade',
+        value: 'queen',
+        color: 'black'
+    },
+    {
+        cardSuit: 'spade',
+        value: 'king',
+        color: 'black'
+    },
+    {
+        cardSuit: 'spade',
+        value: 'ace',
+        color: 'black'
+    },
+    {
+        cardSuit: 'diamond',
+        value: 6,
+        color: 'red'
+    },
+    {
+        cardSuit: 'diamond',
+        value: 7,
+        color: 'red'
+    },
+    {
+        cardSuit: 'diamond',
+        value: 8,
+        color: 'red'
+    },
+    {
+        cardSuit: 'diamond',
+        value: 9,
+        color: 'red'
+    },
+    {
+        cardSuit: 'diamond',
+        value: 10,
+        color: 'red'
+    },
+    {
+        cardSuit: 'diamond',
+        value: 'jack',
+        color: 'red'
+    },
+    {
+        cardSuit: 'diamond',
+        value: 'queen',
+        color: 'red'
+    },
+    {
+        cardSuit: 'diamond',
+        value: 'king',
+        color: 'red'
+    },
+    {
+        cardSuit: 'diamond',
+        value: 'ace',
+        color: 'red'
+    },
+    {
+        cardSuit: 'heart',
+        value: 6,
+        color: 'red'
+    },
+    {
+        cardSuit: 'heart',
+        value: 7,
+        color: 'red'
+    },
+    {
+        cardSuit: 'heart',
+        value: 8,
+        color: 'red'
+    },
+    {
+        cardSuit: 'heart',
+        value: 9,
+        color: 'red'
+    },
+    {
+        cardSuit: 'heart',
+        value: 10,
+        color: 'red'
+    },
+    {
+        cardSuit: 'heart',
+        value: 'jack',
+        color: 'red'
+    },
+    {
+        cardSuit: 'heart',
+        value: 'queen',
+        color: 'red'
+    },
+    {
+        cardSuit: 'heart',
+        value: 'king',
+        color: 'red'
+    },
+    {
+        cardSuit: 'heart',
+        value: 'ace',
+        color: 'red'
+    },
+    {
+        cardSuit: 'clubs',
+        value: 6,
+        color: 'red'
+    },
+    {
+        cardSuit: 'clubs',
+        value: 7,
+        color: 'red'
+    },
+    {
+        cardSuit: 'clubs',
+        value: 8,
+        color: 'red'
+    },
+    {
+        cardSuit: 'clubs',
+        value: 9,
+        color: 'red'
+    },
+    {
+        cardSuit: 'clubs',
+        value: 10,
+        color: 'red'
+    },
+    {
+        cardSuit: 'clubs',
+        value: 'jack',
+        color: 'red'
+    },
+    {
+        cardSuit: 'clubs',
+        value: 'queen',
+        color: 'red'
+    },
+    {
+        cardSuit: 'clubs',
+        value: 'king',
+        color: 'red'
+    },
+    {
+        cardSuit: 'clubs',
+        value: 'ace',
+        color: 'red'
+    }
+];
+
 
 
 
