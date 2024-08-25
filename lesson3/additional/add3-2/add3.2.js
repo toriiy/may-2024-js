6376790b12e26b8f9b19adae5e311b37ad78a5ab
toriiy <vikaizhak54@gmail.com>
2024-08-25 19:46:13 +0300

    did the add3.2

--- lesson3/additional/add3-2/add3.2.js
+++ lesson3/additional/add3-2/add3.2.js
@@ -149,19 +149,83 @@ console.log('');
 // TO BE CONTINUED .....
 // ]
 
-// let usersWithId = [
-//     {id: 1, name: 'vasya', age: 31, status: false},
-//     {id: 2, name: 'petya', age: 30, status: true},
-//     {id: 3, name: 'kolya', age: 29, status: true},
-//     {id: 4, name: 'olya', age: 28, status: false}
-// ];
-//
-// let citiesWithId = [
-//     {user_id: 3, country: 'USA', city: 'Portland'},
-//     {user_id: 1, country: 'Ukraine', city: 'Ternopil'},
-//     {user_id: 2, country: 'Poland', city: 'Krakow'},
-//     {user_id: 4, country: 'USA', city: 'Miami'}
-// ];
-//
-// let usersWithCities = [];
+let usersWithId = [
+    {id: 1, name: 'vasya', age: 31, status: false},
+    {id: 2, name: 'petya', age: 30, status: true},
+    {id: 3, name: 'kolya', age: 29, status: true},
+    {id: 4, name: 'olya', age: 28, status: false}
+];
 
+let citiesWithId = [
+    {user_id: 3, country: 'USA', city: 'Portland'},
+    {user_id: 1, country: 'Ukraine', city: 'Ternopil'},
+    {user_id: 2, country: 'Poland', city: 'Krakow'},
+    {user_id: 4, country: 'USA', city: 'Miami'}
+];
+
+let usersWithCities = [];
+
+for (const user of usersWithId) {
+    usersWithCities[usersWithCities.length] = user;
+}
+
+for (const city of citiesWithId) {
+    for (const user of usersWithCities) {
+        if (city.user_id === user.id) {
+            user['address'] = city;
+        }
+    }
+}
+console.log(usersWithCities);
+console.log('');
+
+// - Взяти масив з 10 чисел або створити його. Вивести в консоль тільки ті елементи, значення яких є парними.
+
+let arrWithNum = [0, 66, 455, -68, 33, 81, 673, 96, -56, 345];
+console.log(arrWithNum);
+
+for (const number of arrWithNum) {
+    if (number % 2 === 0) {
+        console.log(number);
+    }
+}
+console.log('');
+
+// - Взяти масив з 10 чисел або створити його. Створити 2й порожній масив. За допомогою будь-якого циклу скопіювати значення одного масиву в інший.
+
+let arrWithNum2 = [];
+
+for (const number of arrWithNum) {
+    arrWithNum2[arrWithNum2.length] = number;
+}
+console.log(arrWithNum2);
+console.log('');
+
+// - Дано масив: [ 'a', 'b', 'c'] . За допомогою циклу for зібрати всі букви в слово.
+// - Дано масив: [ 'a', 'b', 'c'] . За допомогою циклу while зібрати всі букви в слово.
+// - Дано масив: [ 'a', 'b', 'c'] . За допомогою циклу for of зібрати всі букви в слово.
+
+let letters = ['a', 'b', 'c'];
+
+let word1 = '';
+
+for (let i = 0; i < letters.length; i++) {
+    word1 += letters[i];
+}
+console.log(word1);
+
+let word2 = '';
+
+let i = 0;
+while (i < letters.length) {
+    word2 += letters[i];
+    i++;
+}
+console.log(word2);
+
+let word3 = '';
+
+for (const letter of letters) {
+    word3 += letter;
+}
+console.log(word3);
